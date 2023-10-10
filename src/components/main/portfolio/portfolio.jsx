ce3523d76ef790a12ff57ba18db02c1ff44584f0
Alex Ryabich <alexryabich@mail.ru>
2023-10-10 22:27:06 +0300

    исправления 2.0

--- src/components/main/portfolio/portfolio.jsx
+++ src/components/main/portfolio/portfolio.jsx
@@ -8,22 +8,31 @@ function Portfolio() {
       <h2 className="portfolio__title">Портфолио</h2>
       <ul className="portfolio__list">
         <li className="portfolio__list-item">
-          <a href="https://github.com/TexnaRRR/how-to-learn" target="_blank" className="portfolio__item-text hover-effect link-effect">Статичный сайт</a>
-          <a href="https://github.com/TexnaRRR/how-to-learn"  target="_blank" className="portfolio__link hover-effect link-effect">
-            <img src={portfolioLink} alt="Ссылка" className="portfolio__link-image"/>
+          <a href="https://github.com/TexnaRRR/how-to-learn" target="_blank" className="portfolio__item-text hover-effect link-effect">
+            <span>Статичный сайт</span>
+          <img src={portfolioLink} alt="Ссылка" className="portfolio__link-image"/>
           </a>
+          {/* <a href="https://github.cm/TexnaRRR/how-to-learn"  target="_blank" className="portfolio__link hover-effect link-effect">
+            <img src={portfolioLink} alt="Ссылка" className="portfolio__link-image"/>
+          </a> */}
         </li>
         <li className="portfolio__list-item">
-          <a href="https://texnarrr.github.io/russian-travel/" target="_blank" className="portfolio__item-text hover-effect link-effect">Адаптивный сайт</a>
-          <a href="https://texnarrr.github.io/russian-travel/" target="_blank" className="portfolio__link hover-effect link-effect">
+          <a href="https://texnarrr.github.io/russian-travel/" target="_blank" className="portfolio__item-text hover-effect link-effect">
+            <span>Адаптивный сайт</span>
+          <img src={portfolioLink} alt="Ссылка" className="portfolio__link-image"/>
+          </a>
+          {/* <a href="https://texnarrr.github.io/russian-travel/" target="_blank" className="portfolio__link hover-effect link-effect">
             <img src={portfolioLink} alt="Ссылка" className="portfolio__link-image"/>
-            </a>
+            </a> */}
         </li>
         <li className="portfolio__list-item">
-          <a href="https://github.com/TexnaRRR/react-mesto-api-full-gha" target="_blank" className="portfolio__item-text hover-effect link-effect">Одностраничное приложение</a>
-          <a href="https://github.com/TexnaRRR/react-mesto-api-full-gha" target="_blank" className="portfolio__link hover-effect link-effect">
-            <img src={portfolioLink} alt="Ссылка" className="portfolio__link-image"/>
+          <a href="https://github.com/TexnaRRR/react-mesto-api-full-gha" target="_blank" className="portfolio__item-text hover-effect link-effect">
+            <span>Одностраничное приложение</span>
+          <img src={portfolioLink} alt="Ссылка" className="portfolio__link-image"/>
           </a>
+          {/* <a href="https://github.com/TexnaRRR/react-mesto-api-full-gha" target="_blank" className="portfolio__link hover-effect link-effect">
+            <img src={portfolioLink} alt="Ссылка" className="portfolio__link-image"/>
+          </a> */}
         </li>
       </ul>
     </section>
